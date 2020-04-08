e63de034a792c2d2da640b2e5abb0915d6ed2414
Kaite Kelly <murphy.kaite@gmail.com(opens in new tab)>
2020-04-08 16:21:18 -0700

    adding source files to github repository

---
+++ Develop/script.js
@@ -0,0 +1,18 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+
+let answer = confirm("Do you like apples?");
+console.log(answer)
