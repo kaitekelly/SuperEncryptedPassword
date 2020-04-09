a9140aafeb57b976dfb246741ea9a24cbc985fd4
Kaite Kelly <murphy.kaite@gmail.com(opens in new tab)>
2020-04-08 22:47:08 -0700

    updated js with number, letter, and character arrays

--- Develop/script.js
+++ Develop/script.js
@@ -1,10 +1,14 @@
+let numArr = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+let letterArr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+let charArr  = ["!", "#", "$", "%", "&", "(", ")", "*", "+", "-", "/", ":", ";", "<", "=", ">", "?", "@", "~"]
+
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+let generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  let password = generatePassword();
+  let passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
